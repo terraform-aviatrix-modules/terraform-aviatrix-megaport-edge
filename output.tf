959d1e47f0a5ea2dadf595de74c6f15acd2484f4
Dennizz <root@ipaddr.nl>
2024-12-12 10:52:06 +0100

    add MVE's as output

--- output.tf
+++ output.tf
@@ -5,3 +5,11 @@ output "management_ip" {
 output "ha_management_ip" {
   value = local.hagw_management_prefix
 }
+
+output "mve" {
+  value = megaport_mve.default
+}
+
+output "ha_mve" {
+  value = var.ha_gw ? megaport_mve.ha_gw : null
+}
